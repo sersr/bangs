35ea875dc34c81784bf17f7488798818823a7b84
sersr <1334349436@qq.com>
2022-06-19 16:37:13 +0800

    消除 提示

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,10 +5,12 @@ import 'package:flutter/services.dart';
 import 'package:bangs/bangs.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
